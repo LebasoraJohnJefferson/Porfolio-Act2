45f9c0082be5fa92d5ee8ec5adb9092a75b6d865
LebasoraJohnJefferson <johnjefferson.lebasora@evsu.edu.ph>
2021-09-25 05:58:45 -0700

    minior changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
-  constructor() { }
+  @Input() year:number= new Date().getFullYear();;
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
